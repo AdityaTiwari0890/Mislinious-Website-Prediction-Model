f7e83ac6e6665e3332faac461e23c005ffce0ca6
Aditya Tiwari <aktiwari089081@gmail.com>
2025-12-14 13:02:51 +0530

    Add initial imports for model training and evaluation

---
+++ models/lib.py
@@ -0,0 +1,10 @@
+import os
+print("Current dir:", os.getcwd())
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+import joblib
